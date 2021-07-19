6fbdf02f5cba7ef50c162db09f6fe095204a0440
Oleg <roxvemm2014@gmail.com>
2021-07-18 21:45:55 -0700

    initial commit

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -17,9 +17,28 @@ function QuestionForm(props) {
     });
   }
 
+  // make a fetch request triggered by submit button 
+  // send POST request formated per API documentation
+  // convert data into string/test via JSON.stringify method    
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        prompt: formData.prompt,
+        answers: [
+          formData.answer1,
+          formData.answer2,
+          formData.answer3,
+          formData.answer4,
+        ],
+        correctIndex: parseInt(formData.correctIndex),
+      }),
+    });
   }
 
   return (
